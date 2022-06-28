dbe88a93a3e4467462e652176f60e614c1f20ab0
lakshayZebpay <lakshay.t@zebpay.com>
2022-06-28 14:30:38 +0530

    imporvements on the main page calls folder created

--- src/CryptoCoins/Transactions/Transactions.js
+++ src/CryptoCoins/Transactions/Transactions.js
@@ -2,16 +2,46 @@ import { Card } from "react-bootstrap";
 
 const Transactions = (props) => {
   const data = props.cryptoTransactions.map((transaction) => {
+    const borderColor = () => {
+      const progress = transaction.progress;
+      if (progress === "Completed") {
+        return "primary";
+      } else if (progress === "Rejected") {
+        return "danger";
+      } else if (progress === "partiallyFilled") {
+        return "info";
+      } else {
+        return "dark";
+      }
+    };
+
+    const transcationMessage = () => {
+      const progress = transaction.progress;
+      if (progress === "Completed") {
+        return `Bought ${transaction.coinAmount} for $ ${transaction.cost}`;
+      } else if (progress === "Rejected") {
+        return `Transaction of ${transaction.coinAmount} for $ ${transaction.cost} has been Rejected`;
+      } else if (progress === "partiallyFilled") {
+        return `Transaction of ${transaction.coinAmount} for $ ${transaction.cost} has been Partially Filled`;
+      } else {
+        return `Transaction of ${transaction.coinAmount} for $ ${transaction.cost} is still pending`;
+      }
+    };
+
     return (
-      <Card key={transaction.id} style={{ marginBottom: "5px" }}>
+      <Card
+        border={borderColor()}
+        key={transaction.id}
+        style={{ marginBottom: "5px", borderLeftWidth: "4px" }}
+      >
         <Card.Header>{transaction.exchangeName}</Card.Header>
         <Card.Body>
           <Card.Title>{transaction.coinName}</Card.Title>
-          <Card.Text>
-            {`Bought ${transaction.coinAmount} for $ ${transaction.cost}`}
-          </Card.Text>
+          <Card.Text>{transcationMessage()}</Card.Text>
         </Card.Body>
-        <Card.Footer className="text-muted">{transaction.time}</Card.Footer>
+        <Card.Footer className="text-muted">
+          {transaction.time || "Pending ..."}
+        </Card.Footer>
       </Card>
     );
   });
