e8a34ee06fe869f2c0b4684f52bb81a2913c75e0
pratikshapthfdr <pratiksha@zebpay.com>
2022-06-28 21:23:52 +0530

    wsdfgh

--- src/CryptoCoins/CryptoBuying/CryptoBuying.js
+++ src/CryptoCoins/CryptoBuying/CryptoBuying.js
@@ -5,26 +5,64 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 const CryptoBuying = (props) => {   
-  
-    const [show, setShow] = useState(false);
-
+    const coinsValue = {
+        btc: 21000,
+        ethereum: 1200,
+        bat: 10,
+        ripple: 20,
+      }
+    const [show, setShow] = useState(false); 
+    const [btcValue, setBtcValue] = useState(""); 
+    const [dollarValue, setDollarValue] = useState(""); 
+    const [coinValue, setCoinValue] = useState(21000); 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-  
+    const handleBtcToDollar = (e) => {
+        setBtcValue(e.target.value);
+        setDollarValue(e.target.value * coinValue);
+      }
+      
+      const handleDollarToBtc = (e) => {
+        setBtcValue(e.target.value / coinValue);
+        setDollarValue(e.target.value);
+      } 
     return ( 
     
       <>   
+          
+          <div> 
+<input value={btcValue}   onChange={handleBtcToDollar} type="number" />
+<select> 
 
+  <option value="USD">BTC</option>
 
+</select>  
+</div>  
+<div>
+<input value={dollarValue}   onChange={handleDollarToBtc} type="number" />
+<select> 
 
+<option value="USD">Dollar</option>
 
-          <p>Convert </p> 
+</select>
+  </div>
+          {/* <p>Convert </p>  */}
+          {/* <div>
+          <input value={btcValue}   onChange={setbtcValue} type="number" />
+          <select> 
+          
+            <option value="USD">BTC</option>
+        
+          </select>  
+          </div>  
           <div>
           <input type="number" />
-          <select>
-            <option value="USD">USD</option>
-          </select>
-          </div> 
+          <select> 
+          
+          <option value="USD">BTC</option>
+      
+        </select>
+          </div>     */}
  
         <Button variant="primary" onClick={handleShow}>
             Buy
@@ -50,8 +88,6 @@ const CryptoBuying = (props) => {
     ); 
 };
 export default CryptoBuying 
-
-
 
 
 
