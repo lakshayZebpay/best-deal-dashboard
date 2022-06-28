4f54b744ef97123e7355368935365729197a4a51
pratikshapthfdr <pratiksha@zebpay.com>
2022-06-28 17:17:28 +0530

    erdgtfjg

--- src/CryptoCoins/CryptoBuying/CryptoBuying.js
+++ src/CryptoCoins/CryptoBuying/CryptoBuying.js
@@ -1,18 +1,38 @@
-import React, {useState} from 'react';
+
 import ReactDOM from 'react-dom';
 import "./CryptoBuying.css";
-import history from './history';
-
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 const CryptoBuying = (props) => {   
+  
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
   
     return ( 
     
-    <form> 
-{/* <button>Youtube</button> */} 
- <button variant="btn btn-success"  id="id1" onClick={() => history.push('/buy')}>Buy</button>
- <button variant="outline-secondary">Cancel</button>{' '}
- </form>
-       
+      <>  
+        <Button variant="primary" onClick={handleShow}>
+            Buy
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     
     ); 
 };
